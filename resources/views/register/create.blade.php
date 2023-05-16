54f488539d2a36d8cba9ff046f050669288f10d8
Cesar Sipaque <cesar@developer.com>
2023-05-16 17:39:43 -0600

    arregaldo registro

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -25,14 +25,14 @@
                                 class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column ms-auto me-auto ms-lg-auto me-lg-5">
                                 <div class="card card-plain">
                                     <div class="card-header">
-                                        <h4 class="font-weight-bolder">Sign Up</h4>
-                                        <p class="mb-0">Enter your name, email and password to register</p>
+                                        <h4 class="font-weight-bolder">Registrarse</h4>
+                                        <p class="mb-0">Introduce tu nombre, correo y contrasena</p>
                                     </div>
                                     <div class="card-body">
                                         <form method="POST" action="{{ route('register') }}">
                                             @csrf
                                             <div class="input-group input-group-outline mt-3">
-                                                <label class="form-label">Name</label>
+                                                <label class="form-label">Nombre</label>
                                                 <input type="text" class="form-control" name="name"
                                                     value="{{ old('name') }}">
                                             </div>
@@ -40,7 +40,7 @@
                                             <p class='text-danger inputerror'>{{ $message }} </p>
                                             @enderror
                                             <div class="input-group input-group-outline mt-3">
-                                                <label class="form-label">Email</label>
+                                                <label class="form-label">Correo</label>
                                                 <input type="email" class="form-control" name="email"
                                                     value="{{ old('email') }}">
                                             </div>
@@ -54,26 +54,25 @@
                                             @error('password')
                                             <p class='text-danger inputerror'>{{ $message }} </p>
                                             @enderror
-                                            <div class="form-check form-check-info text-start ps-0 mt-3">
+                                            {{-- <div class="form-check form-check-info text-start ps-0 mt-3">
                                                 <input class="form-check-input" type="checkbox" value=""
                                                     id="flexCheckDefault" checked>
                                                 <label class="form-check-label" for="flexCheckDefault">
                                                     I agree the <a href="javascript:;"
                                                         class="text-dark font-weight-bolder">Terms and Conditions</a>
                                                 </label>
-                                            </div>
+                                            </div> --}}
                                             <div class="text-center">
                                                 <button type="submit"
-                                                    class="btn btn-lg bg-gradient-primary btn-lg w-100 mt-4 mb-0">Sign
-                                                    Up</button>
+                                                    class="btn btn-lg bg-gradient-primary btn-lg w-100 mt-4 mb-0">Registrarse</button>
                                             </div>
                                         </form>
                                     </div>
                                     <div class="card-footer text-center pt-0 px-lg-2 px-1">
                                         <p class="mb-2 text-sm mx-auto">
-                                            Already have an account?
+                                            Ya tienes cuenta?
                                             <a href="{{ route('login') }}"
-                                                class="text-primary text-gradient font-weight-bold">Sign in</a>
+                                                class="text-primary text-gradient font-weight-bold">Ingresar</a>
                                         </p>
                                     </div>
                                 </div>
